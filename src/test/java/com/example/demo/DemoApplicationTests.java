9975697d366848e575cd084ccbe3fe7a9f3f8980
Manu <manuelmoualek@gmail.com>
2022-03-24 17:16:36 +0000

    initial commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,28 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoApplicationTests {
 
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddNumber() {
+		// given
+		int numberOne = 20;
+		int numberTwo = 30;
+
+		// when
+		int result = underTest.add(numberOne, numberTwo);
+
+		//then
+
+
+	}
+
+	static class Calculator{
+		int add(int a, int b){
+			return a + b;
+		}
 	}
 
 }
