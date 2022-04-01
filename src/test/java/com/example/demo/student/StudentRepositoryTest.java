15e8fdca4f87dbfea498717d29e4573c1f158f50
Manu <manuelmoualek@gmail.com>
2022-04-01 13:28:51 +0100

    changed method name in test

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,17 +1,25 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.*;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTest;
 
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
     @Test
-    void itShouldCheckIfStudentExistsEmail() {
+    void itShouldCheckIfStudentEmailExists() {
      //given
         Student student = new Student(
                 "Manu",
@@ -25,6 +33,19 @@ class StudentRepositoryTest {
 
      //then
         assertThat(expected).isTrue();
+
+    }
+
+    @Test
+    void itShouldCheckIfStudentEmailDoesNotExist() {
+        //given
+        String email = "manu@gmail.com";
+
+        //when
+        boolean expected = underTest.selectExistsEmail(email);
+
+        //then
+        assertThat(expected).isFalse();
 
     }
 }
